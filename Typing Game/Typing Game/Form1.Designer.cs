09d36877e976c1b015d70dcfb1a62f4fcd0c9d27
Knight <82474150+RealKnight14@users.noreply.github.com>
2022-11-28 10:42:14 +0000

    Add files via upload

---
+++ Typing Game/Typing Game/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace Typing_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblright = new System.Windows.Forms.Label();
+            this.lblwrong = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblword = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblright
+            // 
+            this.lblright.AutoSize = true;
+            this.lblright.BackColor = System.Drawing.Color.Lime;
+            this.lblright.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblright.Location = new System.Drawing.Point(12, 19);
+            this.lblright.Name = "lblright";
+            this.lblright.Size = new System.Drawing.Size(134, 31);
+            this.lblright.TabIndex = 0;
+            this.lblright.Text = "Correct: 0";
+            // 
+            // lblwrong
+            // 
+            this.lblwrong.AutoSize = true;
+            this.lblwrong.BackColor = System.Drawing.Color.Red;
+            this.lblwrong.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblwrong.Location = new System.Drawing.Point(657, 22);
+            this.lblwrong.Name = "lblwrong";
+            this.lblwrong.Size = new System.Drawing.Size(131, 29);
+            this.lblwrong.TabIndex = 1;
+            this.lblwrong.Text = "Incorrect: 0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(304, 363);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(214, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Press Enter To Check The Text With Word ";
+            // 
+            // lblword
+            // 
+            this.lblword.AutoSize = true;
+            this.lblword.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblword.Location = new System.Drawing.Point(374, 123);
+            this.lblword.Name = "lblword";
+            this.lblword.Size = new System.Drawing.Size(82, 31);
+            this.lblword.TabIndex = 3;
+            this.lblword.Text = "Word";
+            this.lblword.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(265, 232);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 38);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.checkGame);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblwrong);
+            this.Controls.Add(this.lblright);
+            this.Name = "Form1";
+            this.Text = "Typing Game - AE";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblright;
+        private System.Windows.Forms.Label lblwrong;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblword;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Typing Game/Typing Game/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Typing_Game
+{
+    public partial class Form1 : Form
+    {
+        string[] words = {"internet", "programming", "mooict", "coding", "sky", "banana"};
+
+        Random rnd = new Random();
+
+        int correct = 0;
+        int Incorrect = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            lblword.Text = words[rnd.Next(0,words.Length) ];
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+
+        }
+        private void checkGame(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                if (textBox1.Text == lblword.Text)
+                    correct++;
+                lblword.Text = words[rnd.Next(0, words.Length)];
+                textBox1.Text = null;
+            }
+            else
+            {
+                Incorrect++;
+                lblword.Text = words[rnd.Next(0, words.Length)];
+                textBox1.Text = null;
+            }
+
+            lblright.Text = "correct:" + correct;
+            lblwrong.Text = "incorrect" + Incorrect;
+        }
+    }
+}
